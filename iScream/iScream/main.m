c8fa599d436b1562833352dbfde5531824d8796c
Paul de Lange <pauldelange@pauls-mbp.home>
2014-05-14 17:49:42 +1000

    empty project

---
+++ iScream/iScream/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iScream
+//
+//  Created by Paul de Lange on 14/05/2014.
+//  Copyright (c) 2014 Gilmert Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
